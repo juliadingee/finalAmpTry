fb3b13bee7c6f5b3e4c62c8347890f6ce3ee4d07
juliadingee <123409538+juliadingee@users.noreply.github.com>
2024-12-12 19:58:15 -0700

    added signout

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
-import { Authenticator } from '@aws-amplify/ui-react';
+
 import outputs from "../amplify_outputs.json";
+import '@aws-amplify/ui-react/styles.css';
+
+import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
 Amplify.configure(outputs);
