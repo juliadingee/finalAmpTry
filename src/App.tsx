a9bd722f65ae4ecee6e51e6a2cdaddb418ab574d
juliadingee <123409538+juliadingee@users.noreply.github.com>
2024-12-12 20:40:55 -0700

    j

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
+import { useAuthenticator } from '@aws-amplify/ui-react';
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 
-import { useAuthenticator } from '@aws-amplify/ui-react';
+
 
 const client = generateClient<Schema>();
 
